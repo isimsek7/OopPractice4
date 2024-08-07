d9765fb5883af4a1d8581fbb8511beb19bbe0df0
isimsek7 <isimsekmusic@gmail.com>
2024-08-07 23:07:17 +0300

    Add files via upload

---
+++ EmptyClass.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace ClassObj
+{
+	public class Araba
+	{
+		public string _marka;
+		public string _model;
+		public string _renk;
+		private int _kapiSayisi;
+
+		public int KapiSayisi
+		{
+			get
+			{
+				return _kapiSayisi;
+			}
+			set
+			{
+				if (value == 2 || value == 4)
+				{
+
+					_kapiSayisi = value;
+				}
+				else
+				{
+					Console.WriteLine("Gecersiz kapi sayisi.");
+					_kapiSayisi = -1;
+				}
+						
+			}
+		}
+	public void Ozet()
+		{
+			Console.WriteLine($"{_kapiSayisi} kapili {_renk} {_marka} {_model}");
+		}
+	}
+}
+	
+
+
+
+
+
+
+
+
+    // Marka, Model, Renk, Kapı Sayısı
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using ClassObj;
+
+Araba araba1 = new Araba();
+araba1._marka = "Murat";
+araba1._model = "131";
+araba1._renk = "Beyaz";
+araba1.KapiSayisi = 3;
+araba1.Ozet();
